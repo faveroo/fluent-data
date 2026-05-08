8b5e827031d65526a4012b9bfc5a30f55bca96ba
faveroo <gabriel290974@gmail.com>
2026-05-08 09:07:11 -0300

    feat: Criação da injeção de dependencia

---
+++ src/Container/Container.php
@@ -0,0 +1,82 @@
+<?php
+
+namespace Gabriel\FluentData\Container;
+
+use Closure;
+use Exception;
+use ReflectionClass;
+use ReflectionParameter;
+
+class Container
+{
+    protected array $bindings = [];
+
+    protected array $instances = [];
+
+    public function bind(
+        string $abstract,
+        Closure|string $concrete
+    ): void {
+        $this->bindings[$abstract] = $concrete;
+    }
+
+    public function singleton(
+        string $abstract,
+        Closure|string $concrete
+    ): void {
+        $this->instances[$abstract] = $this->resolve($concrete);
+    }
+
+    public function make(string $abstract): mixed 
+    {
+        if(isset($this->instances[$abstract])) {
+            return $this->instances[$abstract];
+        }
+
+        $concrete = $this->bindings[$abstract] ?? $abstract;
+
+        return $this->resolve($concrete);
+    }
+
+    public function resolve(
+        Closure|string $concrete
+    ): mixed {
+        if ($concrete instanceof Closure) {
+            return $concrete($this);
+        }
+
+        $reflection = new ReflectionClass($concrete);
+
+        if(!$reflection->isInstantiable()) {
+            throw new Exception("Class {$concrete} is not instantiable.");
+        }
+
+        $constructor = $reflection->getConstructor();
+
+        if(!$constructor) {
+            return new $concrete;
+        }
+
+        $dependecies = array_map(
+            fn (ReflectionParameter $param) =>
+                $this->resolveDependency($param),
+            $constructor->getParameters()
+        );
+
+        return new $concrete(...$dependecies);
+    }
+
+    protected function resolveDependency(
+        ReflectionParameter $parameter
+    ): mixed {
+        $type = $parameter->getType();
+
+        if(!$type) {
+            throw new Exception(
+                "Cannot resolve parameter {$parameter->getName()}"
+            );
+        }
+
+        return $this->make($type->getName());
+    }
+}
