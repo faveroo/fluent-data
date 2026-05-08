1cdeade1f95ce7feea1ce1d94a08b8a0bf9e966b
faveroo <gabriel290974@gmail.com>
2026-05-08 10:39:46 -0300

    feat: add string manipulation methods for improved utility

--- src/Support/Str.php
+++ src/Support/Str.php
@@ -11,6 +11,57 @@ class Str
         );
     }
 
+    public static function studly(string $value): string
+    {
+        $value = str_replace(
+            ['-', '_'],
+            ' ',
+            $value
+        );
+
+        $value = ucwords($value);
+
+        return str_replace(' ', '', $value);
+    }
+
+    public static function camel(string $value): string
+    {
+        return lcfirst(
+            static::studly($value)
+        );
+    }
+
+    public static function snake(string $value): string
+    {
+        return strtolower(
+            preg_replace(
+                '/(.)(?=[A-Z])/u',
+                '$1_',
+                $value
+            )
+        );
+    }
+
+    public static function startsWith(
+        string $haystack,
+        string $needle
+    ): bool {
+        return str_starts_with(
+            $haystack,
+            $needle
+        );
+    }
+
+    public static function endsWith(
+        string $haystack,
+        string $needle
+    ): bool {
+        return str_ends_with(
+            $haystack,
+            $needle
+        );
+    }
+
     public static function contains(
         string $haystack,
         string $needle
