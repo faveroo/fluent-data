64402ce0e5dd5aaacfcd0a49c8400258ede9b26f
faveroo <gabriel290974@gmail.com>
2026-05-08 10:39:23 -0300

    feat: implement ArrayAccess, Countable, and IteratorAggregate interfaces in Collection class

--- src/Collections/Collection.php
+++ src/Collections/Collection.php
@@ -2,12 +2,23 @@
 
 namespace Gabriel\FluentData\Collections;
 
+use ArrayAccess;
+use ArrayIterator;
+use Countable;
+use IteratorAggregate;
+
 use Gabriel\FluentData\Collections\Traits\EnumeratesValues;
 use Gabriel\FluentData\Collections\Traits\Macroable;
 use Gabriel\FluentData\Contracts\Arrayable;
 use Gabriel\FluentData\Support\Fluent;
+use Override;
+use Traversable;
 
-class Collection extends Fluent implements Arrayable
+class Collection extends Fluent implements 
+    Arrayable,
+    ArrayAccess,
+    Countable,
+    IteratorAggregate
 {
     use Macroable, EnumeratesValues;
 
@@ -18,6 +29,43 @@ class Collection extends Fluent implements Arrayable
         $this->items = $items;
     }
 
+    public function __get(string $key): mixed
+    {
+        return new HighOrderCollectionProxy(
+            $this,
+            $key
+        );
+    }
+
+    public function count(): int
+    {
+        return count($this->items);
+    }
+
+    public function getIterator(): Traversable
+    {
+        return new ArrayIterator($this->items);
+    }
+
+    public function offsetExists($offset): bool
+    {
+        return isset($this->items[$offset]);
+    }
+
+    public function offsetGet($offset): mixed
+    {
+        return $this->items[$offset];
+    }
+
+    public function offsetSet($offset, $value): void
+    {
+        $this->items[$offset] = $value;
+    }
+
+    public function offsetUnset($offset): void
+    {
+        unset($this->items[$offset]);
+    }
 
     public function toArray(): array
     {
