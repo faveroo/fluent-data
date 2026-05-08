657a867d904c594d05e4618fa399bd1e104bd07d
faveroo <gabriel290974@gmail.com>
2026-05-08 10:39:31 -0300

    feat: add helper functions dd, tap, and value for improved utility

--- src/helpers.php
+++ src/helpers.php
@@ -8,3 +8,33 @@ if(!function_exists('collect')) {
         return new Collection($items);
     }
 }
+
+if(!function_exists('dd')) {
+    function dd(mixed ...$vars): never
+    {
+        var_dump(...$vars);
+
+        die();
+    }
+}
+
+if(!function_exists('tap')) {
+    function tap(
+        mixed $value,
+        callable $callback
+    ): mixed {
+
+        $callback($value);
+
+        return $value;
+    }
+}
+
+if(!function_exists('value')) {
+    function value(mixed $value): mixed 
+    {
+        return $value instanceof Closure
+            ? $value()
+            : $value;
+    }
+}
