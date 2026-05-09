1b1289d4dc36503ba39c342db66294ca7da30416
Gabriel Favero <gabriel290974@gmail.com>
2026-05-09 15:36:45 -0300

    feat: add database configuration file for MySQL connection

---
+++ src/config/database.php
@@ -0,0 +1,19 @@
+<?php
+
+require 'vendor/autoload.php';
+
+return [
+    'default' => env('DB_CONNECTION', 'mysql'),
+
+    'connections' => [
+        'mysql' => [
+            'driver' => 'mysql',
+            'host' => env('DB_HOST', 'localhost'),
+            'port' => env('DB_PORT', '3306'),
+            'database' => env('DB_DATABASE', ''),
+            'username' => env('DB_USERNAME', 'root'),
+            'password' => env('DB_PASSWORD', ''),
+            'charset' => env('DB_CHARSET', 'utf8mb4'),
+        ]
+    ]
+];
