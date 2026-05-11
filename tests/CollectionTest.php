3c8522a7f1b59dd90b9b95ff0d068664d76b2e5d
faveroo <gabriel290974@gmail.com>
2026-05-11 16:23:54 -0300

    feat: add unit tests for Collection class methods including macros, map, first, and last

---
+++ tests/CollectionTest.php
@@ -0,0 +1,61 @@
+<?php
+
+use Gabriel\FluentData\Collections\Collection;
+use PHPUnit\Framework\TestCase;
+
+class CollectionTest extends TestCase
+{
+    protected array $items = [
+        ['name' => 'Abacate', 'price' => 10],
+        ['name' => 'Manga', 'price' => 20],
+    ];
+
+    public function test_collection_macros(): void
+    {
+        Collection::macro('sumPrices', function() {
+            return array_sum(
+                array_column($this->items, 'price')
+            );
+        });
+
+        $result = Collection::make($this->items)->sumPrices();
+
+        $this->assertSame(
+            30,
+            $result,   
+        );
+    }
+
+    public function test_collection_map_function(): void
+    {
+        $result = Collection::make($this->items)
+            ->map(function ($item) {
+                $item['price'] *= 2;
+                return $item;
+            });
+
+        $this->assertSame(
+            [
+                ['name' => 'Abacate', 'price' => 20],
+                ['name' => 'Manga', 'price' => 40],
+            ],
+            $result->all()
+        );
+    }
+
+    public function test_collection_fist_and_last_function(): void
+    {
+        $first = Collection::make($this->items)->first();
+        $end = Collection::make($this->items)->last();
+
+        $result = [$first, $end];
+
+        $this->assertSame(
+            [
+                ['name' => 'Abacate', 'price' => 10],
+                ['name' => 'Manga', 'price' => 20],
+            ],
+            $result
+        );
+    }
+}
